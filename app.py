de0997ea334fcad45670b0557700ed4fb90869a5
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-07-24 17:28:38 -0400

    Completed:Step_One

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from boggle import Boggle
+from flask import Flask, render_template, redirect, request, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "DEUSVULT"
+toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def board_screen():
+    board = boggle_game.make_board()
+    session["board"] = board
+    return render_template('board.html', board=board)
